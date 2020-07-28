3335ac3810ddc7ebdf01d0a34be8704a8516cbb1
John Williams <43114585+johntango@users.noreply.github.com>
2020-07-28 08:11:09 -0400

    Add files via upload

---
+++ server0.js
@@ -0,0 +1,17 @@
+var express = require("express");
+
+var app = express();
+app.get("/", function (req, res) {
+  res.send("<h1> Hello World </h1>");
+});
+
+app.get("/input/:name/:email", function (req, res) {
+  console.log(req.params.name + ", " + req.params.email);
+  res.send({
+    name: req.params.name,
+    email: req.params.email,
+  });
+});
+
+app.listen(3000);
+console.log("Running on port 3000");
