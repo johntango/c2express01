afc98b911807e93c27facb5b37d4fca5f01a9b8a
John Williams <jrwtango@gmail.com>
2020-09-26 11:52:27 -0400

    test1

--- server.test.js
+++ server.test.js
@@ -1,8 +1,26 @@
 const request = require("supertest");
 const app = require("./serverApp");
 describe("Test the root path", () => {
-  test("It should response the GET method", async () => {
+  test("It should response the GET method", async (done) => {
     const response = await request(app).get("/");
     expect(response.statusCode);
+    done();
+  });
+});
+describe("POST /contact", function () {
+  test("contact.name should be john", async (done) => {
+    request(app)
+      .post("/contact")
+      .send({ name: "john", email: "john@mit.edu" }) //json
+      .set("Accept", "application/json")
+      .expect(function (res) {
+        res.body.id = "some fixed id";
+        res.body.name = "john";
+      })
+      .expect(200, {
+        id: "some fixed id",
+        name: "john",
+      });
+    done();
   });
 });
